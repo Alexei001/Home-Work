0be1489233bba631b145ef5f92644773b277b0a9
Alexei Vacari <47106150+Alexei001@users.noreply.github.com>
2019-10-04 14:46:05 +0300

    add HM11 Js

---
+++ Home work 11 (Js)/script.js
@@ -0,0 +1,50 @@
+var days_of_week=prompt("Enter the day of the week?");
+
+console.log('You choosed:', days_of_week);
+
+/*switch(days_of_week) {
+    case 'Monday':
+        alert('Working day');
+        break;
+    case  'Tuesday':
+        alert('Working day');
+        break;
+    case  'Wednesday':
+        alert('Working day');
+        break;
+    case  'Thursday':
+        alert('Working day');
+        break;
+    case  'Friday':
+        alert('Working day');
+        break;
+    case  'Saturday':
+        alert('Weekend ');
+        break;
+    case  'Sunday':
+        alert('Weekend');
+        break;
+    default:
+        alert("Sorry " + days_of_week + " can not define, please try another value");
+}*/
+
+/*if ( days_of_week=='Monday' ){
+    alert('Working day');
+    
+} else if (days_of_week=='Tuesday'){
+    alert( 'Working day');
+} else if (days_of_week=='Wendnesday'){
+    alert( 'Working day');
+} else if (days_of_week=='Thursday'){
+    alert( 'Working day');
+} else if (days_of_week=='Friday'){
+    alert( 'Working day');
+} else if (days_of_week=='Saturday'){
+    alert( 'Weekend');
+} else if (days_of_week=='Sunday'){
+    alert( 'Weekend');
+    
+} else {
+    alert('Can not define, please try another value');
+    
+}*/
