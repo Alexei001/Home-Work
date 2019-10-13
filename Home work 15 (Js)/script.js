4e50018d00f49750a3d008c5655f48a8185527f2
Alexei Vacari <47106150+Alexei001@users.noreply.github.com>
2019-10-13 12:31:58 +0300

    update home work 15

--- Home work 15 (Js)/script.js
+++ Home work 15 (Js)/script.js
@@ -3,18 +3,28 @@
   var valueB = parseInt(prompt('please enter value B'));
   var valueC = parseInt(prompt('please enter value C'));
 
+function getDiscriminant(){
+    return ((valueB * valueB)-(4 * valueA * valueC));
+}
+var disc = getDiscriminant()
 
-function squareEquation(){
-    let   Dis =((valueB * valueB)-(4 * valueA * valueC));
-    if (Dis > 0 ){
-    let   numberX1 = ((- valueB + Math.sqrt(Dis))/(2* valueA));
-    let   numberX2 = ((- valueB - Math.sqrt(Dis))/(2* valueA));
+function squareEquation(discriminant){ //dicriminant-сшитается как (а, б , с)
+    if (discriminant > 0 ){
+    let   numberX1 = ((- valueB + Math.sqrt(discriminant))/(2* valueA));
+    let   numberX2 = ((- valueB - Math.sqrt(discriminant))/(2* valueA));
      alert('Equation has two roots X1: '+ numberX1 +'  '+'X2: '+ numberX2);
-    } else if (Dis == 0){
+     return{
+         numberX1,
+         numberX2
+     }
+    } else if (discriminant == 0){
         let numberX = (-((valueB)/(2*valueA)));
         alert('Equation has only one root X: ' + numberX);
+        return {
+            numberX
+        }
     } else {
-        alert('no valid Discriminant : ' + Dis +' (it has to be positive)');
+        alert('no valid Discriminant : ' + discriminant +' (it has to be positive)');
     }
 }   
-squareEquation();
+squareEquation(disc); //здесь уже задаем параметры (а, б, с)---(1, 2, 3) при вызове функцийб тоесть disc= у нас расчитывается на верху
