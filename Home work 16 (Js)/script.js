9316ace26dabcc2efd136274db78ed92e313691b
Alexei Vacari <47106150+Alexei001@users.noreply.github.com>
2019-10-19 10:41:38 +0300

    add new home work

---
+++ Home work 16 (Js)/script.js
@@ -0,0 +1,28 @@
+// 2.1 Create a function that return fullName
+
+/*function fullName(name){
+    return name;
+}
+
+result = fullName('Alexei Vacari');
+console.log(result);*/
+// 2.2 Create a greeting function that accepts fullName param, 
+//and return a greeting message
+
+/*function hello(fullName){
+    return "Hello! " + fullName;
+}
+
+console.log(hello('Alex Vacari'));*/
+
+//2.3 Create a function that accepts a number and reverses it;
+
+/*function reverseNumber(n){
+        n = n + "";
+        return n.split("").reverse().join("");
+}
+ console.log(reverseNumber(156461));*/
+ /*3. Eslint install
+  3.1 Configuration
+  3.2 Check some rules
+  3.3 Try to fix*/
