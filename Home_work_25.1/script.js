fb9cdda0f1bf79d2a0d9d7c2d8989ed5af42fa16
Alexei Vacari <47106150+Alexei001@users.noreply.github.com>
2019-11-23 10:44:18 +0200

    update home work

--- Home_work_25.1/script.js
+++ Home_work_25.1/script.js
@@ -1,43 +1,42 @@
  
-
-
-/* async function getPOD(dates, renderComponent) {
-        const result = await fetch(`#https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${dates}&hd=true`);
-        const data = await result.json();
-        renderComponent(data);
-    }
-    
-    
-    function renderPODComponent(data) {
-        const { url, copyright, date,  title} = data;
-        const container = document.getElementById('pod-list');
-
-        const titleComponent = document.createElement('h2');
-        titleComponent.innerText = `${title} : ${date}`;
-
-        const imageComponent = document.createElement('img');
-        imageComponent.src = url;
-        imageComponent.style.width = '90%'; 
-        imageComponent.style.height = '80%'; 
-
-        const copyrightComponent = document.createElement('p');
-        copyrightComponent.innerText = copyright;
-        container.append(...[titleComponent,imageComponent, copyrightComponent])
-    } */
-    
     let today =new Date();
     let year= today.getFullYear(), month = today.getMonth(), Date1 = today.getDate();
-    
-     function prevDay(){
-       showSlider(--Date1);
+    let Day = `${year}-${month+1}-${Date1}`;
+    console.log(Day);
+    let arrDay = Day.split('-');
+    //console.log(arrDay);
+
+
+    function prevDay(){
+      Date1 = --Date1;
+      //console.log(Date1);
+      Day = `${year}-${month+1}-${Date1}`;
+      //console.log(Day);
+      getPOD(Day, renderPODComponent);
     }
+
     function nextDay(){
-      showSlider(++Date1);
-    } 
-
-
-    function showSlider()
-    console.log(dates);
-     getPOD(dates,renderPODComponent)
-  
-  
+      Date1 = ++Date1;
+      //console.log(Date1);
+      Day = `${year}-${month+1}-${Date1}`;
+      //console.log(Day);
+      getPOD(Day, renderPODComponent);
+    }
+    async function getPOD(Day, renderComponent) {
+      const result = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${Day}`);
+      const data = await result.json();
+      renderComponent(data)
+  }
+   
+  function renderPODComponent(data) {
+      const { url, copyright, title} = data;
+      const container = document.getElementById('pod-list');
+      const titleComponent = document.createElement('h2');
+      titleComponent.innerText = title +':'+Day;
+      const imageComponent = document.createElement('img');
+      imageComponent.src = url;
+      imageComponent.style.width = '200px'; 
+      const copyrightComponent = document.createElement('p');
+      copyrightComponent.innerText = copyright;
+      container.append(...[titleComponent,imageComponent, copyrightComponent])
+  }
